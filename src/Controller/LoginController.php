b034d910fd1e47a97ce2308ed38a6feec04da170
boris7ran <boris.ran7@gmail.com>
2019-10-03 15:47:39 +0200

    feat(login) implement store login

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,12 +2,33 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class LoginController extends AbstractController
 {
     public function index()
     {
         return $this->render('auth\login.html.twig');
+    }
+
+    public function store(Request $request)
+    {
+        $requestUsername = $request->get('name');
+
+        $user = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findOneBy(['username' => $requestUsername]);
+
+        if (!$user) {
+            throw new Exception('No user found with username: ' . $requestUsername );
+        } elseif ($user->getPassword() !== $request->get('password')) {
+            throw new Exception('Incorrect password');
+        }
+
+        return new Response('User found with username: '.$user->getUsername());
     }
 }
