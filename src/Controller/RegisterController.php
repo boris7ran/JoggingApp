26135061d36dc69f1f7f3731dbaef01326e2c11c
boris7ran <boris.ran7@gmail.com>
2019-10-03 14:33:51 +0200

    feat(register): store user in database

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,12 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 
 class RegisterController extends AbstractController
 {
     public function index()
     {
         return $this->render('register.html.twig');
+    }
+
+    public function store(Request $request)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $user = new User();
+        $user->setUsername($request->get('name'));
+        $user->setPassword($request->get('password'));
+
+        $entityManager->persist($user);
+
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$user->getId());
     }
 }
